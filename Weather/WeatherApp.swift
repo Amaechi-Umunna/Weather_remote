c54a46817dc0984845beee5845bac23459f3ca78
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-10-20 04:01:48 +0100

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Amaechi Chukwu on 20/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
