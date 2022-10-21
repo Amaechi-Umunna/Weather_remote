05c3ea2ba04621e027caa9b6f81b250eea007c47
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-10-21 03:56:49 +0100

    Brought in the house image, added different stylings on portions of string using "attributedString"

--- Weather/Views/Main/HomeView.swift
+++ Weather/Views/Main/HomeView.swift
@@ -24,13 +24,12 @@ struct HomeView: View {
                 .frame(maxHeight: .infinity, alignment: .top)
                 .padding(.top, 257)
             
-            VStack {
+            VStack(spacing: -10) {
                 Text("Montreal")
                     .font(.largeTitle)
                 
-                
                 VStack {
-                    Text("19°" + "\n" + "Mostly Clear")
+                   Text(attributedString)
                     
                     Text("H:24°   L:18°")
                         .font(.title3.weight(.semibold))
@@ -41,6 +40,26 @@ struct HomeView: View {
             }
             .padding(.top, 51)
         }
+    }
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+            
+        }
+        
+        if let pipe = string.range(of: " | ") {
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        if let weather = string.range(of: "Mostly Clear") {
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
+        return string
     }
 }
 
