6615b8a91649777c368535e90a7359bfb8081bf5
Umunnabuike <100558243+Umunnabuike@users.noreply.github.com>
2022-10-22 06:08:33 +0100

    Added scrolling animations in the background

--- Weather/Views/Detail/ForecastView.swift
+++ Weather/Views/Detail/ForecastView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ForecastView: View {
+    var bottomSheetTranslationProrated: CGFloat = 1
+    
     var body: some View {
         ScrollView {
             
@@ -16,7 +18,7 @@ struct ForecastView: View {
         .background(Color.bottomSheetBackground)
         .background(Blur(radius: 25, opaque: true))
         .clipShape(RoundedRectangle(cornerRadius: 44))
-        .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1)
+        .innerShadow(shape: RoundedRectangle(cornerRadius: 44), color: Color.bottomSheetBorderMiddle, lineWidth: 1, offsetX: 0, offsetY: 1, blur: 0, blendMode: .overlay, opacity: 1 - bottomSheetTranslationProrated)
         .overlay {
             // MARK: Bottom Sheet Separator
             Divider()
